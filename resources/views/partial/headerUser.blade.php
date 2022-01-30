d2caf380fa366ecba273611419ca20149d805534
Adi Riyanto <44104241+siyaga@users.noreply.github.com>
2022-01-30 22:00:49 +0700

    Login

--- resources/views/partial/headerUser.blade.php
+++ resources/views/partial/headerUser.blade.php
@@ -6,7 +6,7 @@
 
   <ul class="navbar-nav ms-auto">
   @auth
-  <li class="nav-item dropdown">
+  {{-- <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
       Welcome back, {{ auth()->user()->name }}
     </a>
@@ -21,10 +21,25 @@
       
       </li>
     </ul>
+  </li> --}}
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Welcome back, {{ auth()->user()->name }}
+    </a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+      <a class="dropdown-item" href="#"><i class="bi-bi-layout-text-sidebar-reverse"></i>My Dashboard</a></a>
+      <div class="dropdown-divider"></div>
+      
+      <form action="/logout" method="post">
+        @csrf
+        <button type="submit" class="drowpdown-item"><i class="bi-bi-box-arrow-right"></i>Logout</button>
+      </form>
+    </div>
   </li>
+
   @else    
   <li class="nav-item">
-  <a type="button" class="btn btn-outline-primary align-self-center" style="width: 110px; height:36px ;" href="/login">Login</a>
+  <a type="button" class="btn btn-outline-primary align-self-center " style="width: 110px; height:36px ;" href="/login"><i class="bi bi-box-arrow-in-right"></i>Login</a>
   </li>
   @endauth
   </ul>
